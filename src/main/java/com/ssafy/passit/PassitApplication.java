7c56b9551168feff9c5018040fb67f2cc41c2423
yun990325 <yun990325@naver.com>
2026-05-22 11:24:13 +0900

    Init Commit

---
+++ src/main/java/com/ssafy/passit/PassitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.passit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/passit/PassitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.passit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
